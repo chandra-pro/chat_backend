4930750137b1c4de7bddc2acf59da046adcc274d
chandra-pro <goluchandra1812@gmail.com>
2023-08-15 17:33:07 +0530

    model updated

--- models/messageModel.js
+++ models/messageModel.js
@@ -1,20 +1,14 @@
 const mongoose = require("mongoose");
 
-const MessageSchema = mongoose.Schema(
+const messageSchema = mongoose.Schema(
   {
-    message: {
-      text: { type: String, required: true },
-    },
-    users: Array,
-    sender: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    content: { type: String, trim: true },
+    chat: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
+    readBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
 
-module.exports = mongoose.model("Messages", MessageSchema);
+const Message = mongoose.model("Message", messageSchema);
+module.exports = Message;
